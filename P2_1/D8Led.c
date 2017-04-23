4b3223064fa03e61a550b36244b338964ccc1a67
serfati5 <serfati5@users.noreply.github.com>
2017-04-24 01:31:22 +0200

    EC - Práctica 2.1

--- P2_1/D8Led.c
+++ P2_1/D8Led.c
@@ -1,20 +1,4 @@
-/*-------------------------------------------------------------------
-**
-**  Fichero:
-**    D8Led.c  10/6/2014
-**
-**    Estructura de Computadores
-**    Dpto. de Arquitectura de Computadores y Automática
-**    Facultad de Informática. Universidad Complutense de Madrid
-**
-**  Propósito:
-**    Contiene las implementaciones de las funciones
-**    para el acceso al display 8-segmentos de la placa de prototipado
-**
-**  Notas de diseño:
-**    Equivale al fichero homónimo usado en Fundamentos de Computadores 
-**
-**-----------------------------------------------------------------*/
+
 
 #include "44b.h"
 #include "D8Led.h"
@@ -72,9 +56,9 @@ void D8Led_segment(int value)
 {
 	if( (value >= 0) && (value < 8) )
 		// COMPLETAR: debemos escribir en el display (LED8ADDR) el valor
-		// correcto para que se ilumine el segmento que está en la posición
+		// correcto para que se ilumine el segmento que estÃ¡ en la posiciÃ³n
 		// value del array Segmentes. Hay que tener en cuente que el display
-		// usa lógica invertida (0 enciende, 1 apaga).
+		// usa lÃ³gica invertida (0 enciende, 1 apaga).
 
 		LED8ADDR = ~(Segments[value]);
 
@@ -84,9 +68,9 @@ void D8Led_digit(int value)
 {
 	if( (value >= 0) && (value < 16) )
 		// COMPLETAR: debemos escribir en el display (LED8ADDR) el valor
-		// correcto para que se ilumine el dígito que está en la posición
+		// correcto para que se ilumine el dÃ­gito que estÃ¡ en la posiciÃ³n
 		// value del array Digits. Hay que tener en cuente que el display
-		// usa lógica invertida (0 enciende, 1 apaga).
+		// usa lÃ³gica invertida (0 enciende, 1 apaga).
 		LED8ADDR = ~(Digits[value]);
 
 }
