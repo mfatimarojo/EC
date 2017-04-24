402cf1004e9eed0939353fa7cc8de8a03c5817b5
serfati5 <serfati5@users.noreply.github.com>
2017-04-24 17:57:25 +0200

    Update button.h

--- P2_1/button.h
+++ P2_1/button.h
@@ -1,20 +1,3 @@
-/*-----------------------------------------------------------------
-**
-**  Fichero:
-**    button.c  10/6/2014
-**
-**    Estructura de Computadores
-**    Dpto. de Arquitectura de Computadores y Autom�tica
-**    Facultad de Inform�tica. Universidad Complutense de Madrid
-**
-**  Prop�sito:
-**    Contiene las definiciones de los prototipos de funciones
-**    para la gesti�n de los pulsadores de la placa de prototipado
-**
-**  Notas de dise�o:
-**
-**---------------------------------------------------------------*/
-
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
