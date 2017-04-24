8f3d0132df5762a389621e8d090c0b228deec0b9
serfati5 <serfati5@users.noreply.github.com>
2017-04-24 17:56:26 +0200

    Update D8Led.h

--- P2_1/D8Led.h
+++ P2_1/D8Led.h
@@ -1,21 +1,3 @@
-/*-------------------------------------------------------------------
-**
-**  Fichero:
-**    D8Led.h  10/6/2014
-**
-**    Estructura de Computadores
-**    Dpto. de Arquitectura de Computadores y Automática
-**    Facultad de Informática. Universidad Complutense de Madrid
-**
-**  Propósito:
-**    Contiene las declaraciones de los prototipos de funciones
-**    para el acceso al display 8-segmentos de la placa de prototipado
-**
-**  Notas de diseño:
-**    Equivale al fichero homónimo usado en Fundamentos de Computadores 
-**
-**-----------------------------------------------------------------*/
-
 #ifndef D8LED_H_
 #define D8LED_H_
 
