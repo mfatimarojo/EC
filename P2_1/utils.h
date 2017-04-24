2548a16cc8ae885837c8aff5da1c49b3e2cfe374
serfati5 <serfati5@users.noreply.github.com>
2017-04-24 18:00:06 +0200

    EC - Práctica 2.1

--- P2_1/utils.h
+++ P2_1/utils.h
@@ -1,20 +1,4 @@
-/*-------------------------------------------------------------------
-**
-**  Fichero:
-**    utils.h  10/6/2014
-**
-**    Estructura de Computadores
-**    Dpto. de Arquitectura de Computadores y Automática
-**    Facultad de Informática. Universidad Complutense de Madrid
-**
-**  Propósito:
-**    Contiene las definiciones de los prototipos de funciones
-**    auxiliares
-**
-**  Notas de diseño:
-**    Equivale al fichero homónimo usado en Fundamentos de Computadores 
-**
-**-----------------------------------------------------------------*/
+
 
 #ifndef UTILS_H_
 #define UTILS_H_
